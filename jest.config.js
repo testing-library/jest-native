0406f67e2fe5fe62610ab9b784b3d0256265b8fe
Brandon Carroll <brandonvcarroll@gmail.com>
2019-04-04 22:00:42 -0400

    ci: add coverage report

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,4 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.+(js|jsx|ts|tsx)'],
   testMatch: ['**/__tests__/**/*.+(js|jsx|ts|tsx)'],
   testPathIgnorePatterns: [...ignores],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
 };
