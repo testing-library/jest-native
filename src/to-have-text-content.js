9847cfc772312e4cde0a160482a8b72ee9788bd3
gabriel.rohden <gabriel_rohden@outlook.com>
2021-06-19 14:35:14 -0300

    fix: toHaveTextContent collect properly text of children nodes

--- src/to-have-text-content.js
+++ src/to-have-text-content.js
@@ -1,49 +1,30 @@
 import { matcherHint } from 'jest-matcher-utils';
-import compose from 'ramda/src/compose';
-import defaultTo from 'ramda/src/defaultTo';
+import pipe from 'ramda/src/pipe';
+import always from 'ramda/src/always';
 import is from 'ramda/src/is';
 import join from 'ramda/src/join';
-import map from 'ramda/src/map';
-import path from 'ramda/src/path';
-import filter from 'ramda/src/filter';
+import reduce from 'ramda/src/reduce';
+import concat from 'ramda/src/concat';
+import unless from 'ramda/src/unless';
 
 import { checkReactElement, getMessage, matches, normalize } from './utils';
 
-function getText(child, currentValue = '') {
-  let value = currentValue;
+const unlessStringEmpty = unless(is(String), always(''));
 
-  if (!child) {
-    return value;
-  } else if (Array.isArray(child)) {
-    return child.reduce((acc, element) => acc + getText(element), '');
-  } else if (typeof child === 'object') {
-    return getText(path(['props', 'children'], child), value);
-  } else {
-    return `${value}${child}`;
+const collectNormalizedText = pipe(collectChildrenText, join(''), normalize);
+
+function collectChildrenText(element) {
+  if (!element || !element.children) {
+    return [unlessStringEmpty(element)];
   }
+
+  return reduce((texts, child) => concat(texts, collectChildrenText(child)), [], element.children);
 }
 
 export function toHaveTextContent(element, checkWith) {
   checkReactElement(element, toHaveTextContent, this);
 
-  // step 9: enjoy your text content ☺️
-  const textContent = compose(
-    // step 8: strip out extra whitespace
-    normalize,
-    // step 7: join the resulting array
-    join(''),
-    // step 6: filter out values hidden by React
-    filter(child => typeof child === 'string' || typeof child === 'number'),
-    // step 5: map the array to get text content
-    map(child => (typeof child === 'object' ? getText(child) : child)),
-    // step 4: make sure non-array children end up in an array
-    child => (is(Array, child) ? child : [child]),
-    // step 3: default to an array
-    defaultTo([]),
-    // step 2: drill down to the children
-    path(['props', 'children']),
-    // step 1: get the element
-  )(element);
+  const textContent = collectNormalizedText(element);
 
   return {
     pass: matches(textContent, checkWith),
