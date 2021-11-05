0dbda7191106f15a57eda1be10eadf95ad8bd81f
Bruno Castro <bruno.castro@codeminer42.com>
2021-11-05 11:27:12 -0300

    fix(toHaveProp): check null values (#68)

--- src/to-have-prop.js
+++ src/to-have-prop.js
@@ -1,4 +1,4 @@
-import { equals, isNil, not } from 'ramda';
+import { equals } from 'ramda';
 import { matcherHint, stringify, printExpected } from 'jest-matcher-utils';
 import { checkReactElement, getMessage } from './utils';
 
@@ -18,7 +18,7 @@ export function toHaveProp(element, name, expectedValue) {
   const prop = element.props[name];
 
   const isDefined = expectedValue !== undefined;
-  const hasProp = not(isNil(prop));
+  const hasProp = name in element.props;
 
   return {
     pass: isDefined ? hasProp && equals(prop, expectedValue) : hasProp,
