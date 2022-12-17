5cb1a2935c336fbc53b3b793c7a34e3663e30d35
Alexander Chaschin <ac@happynumbers.com>
2022-12-17 13:23:56 +0300

    add comment

--- src/__tests__/types.ts
+++ src/__tests__/types.ts
@@ -4,6 +4,9 @@ import type { JestNativeMatchers } from '../../extend-expect';
 
 interface IsExtendedWithMatchers<R, T extends JestNativeMatchers<R>> {}
 
-// this should not type check if something is wrong with `extend-expect.d.ts`
+/*
+this should not type check if something
+is wrong with jest interfaces extension in `extend-expect.d.ts`
+*/
 type IsJestExplicitGlobalsExtended = IsExtendedWithMatchers<void, Matchers<void>>;
 type IsJestImplicitGlobalsExtended = IsExtendedWithMatchers<void, jest.Matchers<void>>;
