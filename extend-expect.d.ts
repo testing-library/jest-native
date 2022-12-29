c3dc5ce4ba6f9c8eaae391971fd442940f6d1090
Alexander Chaschin <achasch@gmail.com>
2022-12-29 15:09:17 +0300

    fix: change extend-expect types definition to support explicit jest globals (#137)
    Co-authored-by: Maciej Jastrzebski <mdjastrzebski@gmail.com>

--- extend-expect.d.ts
+++ extend-expect.d.ts
@@ -2,23 +2,31 @@ import type { AccessibilityState, ImageStyle, StyleProp, TextStyle, ViewStyle } 
 import type { ReactTestInstance } from 'react-test-renderer';
 import type { AccessibilityValueMatcher } from './src/to-have-accessibility-value';
 
+export interface JestNativeMatchers<R> {
+  toBeDisabled(): R;
+  toBeEmptyElement(): R;
+  toBeEnabled(): R;
+  toBeVisible(): R;
+  toContainElement(element: ReactTestInstance | null): R;
+  toHaveTextContent(text: string | RegExp, options?: { normalizeWhitespace: boolean }): R;
+  toHaveProp(attr: string, value?: unknown): R;
+  toHaveStyle(style: StyleProp<ViewStyle | TextStyle | ImageStyle>): R;
+  toHaveAccessibilityState(state: AccessibilityState): R;
+  toHaveAccessibilityValue(value: AccessibilityValueMatcher): R;
+
+  /** @deprecated This function has been renamed to `toBeEmptyElement`. */
+  toBeEmpty(): R;
+}
+
+// Implicit Jest global `expect`.
 declare global {
   namespace jest {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    interface Matchers<R, T> {
-      toBeDisabled(): R;
-      toBeEmptyElement(): R;
-      toBeEnabled(): R;
-      toBeVisible(): R;
-      toContainElement(element: ReactTestInstance | null): R;
-      toHaveTextContent(text: string | RegExp, options?: { normalizeWhitespace: boolean }): R;
-      toHaveProp(attr: string, value?: unknown): R;
-      toHaveStyle(style: StyleProp<ViewStyle | TextStyle | ImageStyle>): R;
-      toHaveAccessibilityState(state: AccessibilityState): R;
-      toHaveAccessibilityValue(state: AccessibilityValueMatcher): R;
-
-      /** @deprecated This function has been renamed to `toBeEmptyElement`. */
-      toBeEmpty(): R;
-    }
+    interface Matchers<R, T = {}> extends JestNativeMatchers<R> {}
   }
+}
+
+// Explicit `@jest/globals` `expect` matchers.
+declare module '@jest/expect' {
+  interface Matchers<R extends void | Promise<void>> extends JestNativeMatchers<R> {}
 }
---
+++ src/__types__/jest-explicit-extend.test-d.ts
@@ -0,0 +1,15 @@
+// This file checks whether explicit Jest `extend` from '@jest/expect' is correctly extended with Jest Matchers.
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { expect as jestExpect } from '@jest/globals';
+
+jestExpect(null).toBeDisabled();
+jestExpect(null).toBeEmptyElement();
+jestExpect(null).toBeEnabled();
+jestExpect(null).toBeVisible();
+jestExpect(null).toContainElement(null);
+jestExpect(null).toHaveTextContent('');
+jestExpect(null).toHaveProp('foo');
+jestExpect(null).toHaveStyle({});
+jestExpect(null).toHaveAccessibilityState({});
+jestExpect(null).toHaveAccessibilityValue({});
