ffbdc871fb6691ccf3924541650084b9d0ef6cf3
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-01-06 11:17:55 +0100

    fix: add toBeOnTheScreen to exports

--- src/extend-expect.ts
+++ src/extend-expect.ts
@@ -1,13 +1,13 @@
 import { toBeDisabled, toBeEnabled } from './to-be-disabled';
 import { toBeEmptyElement, toBeEmpty } from './to-be-empty-element';
 import { toBeOnTheScreen } from './to-be-on-the-screen';
+import { toBeVisible } from './to-be-visible';
 import { toContainElement } from './to-contain-element';
+import { toHaveAccessibilityState } from './to-have-accessibility-state';
+import { toHaveAccessibilityValue } from './to-have-accessibility-value';
 import { toHaveProp } from './to-have-prop';
 import { toHaveStyle } from './to-have-style';
 import { toHaveTextContent } from './to-have-text-content';
-import { toBeVisible } from './to-be-visible';
-import { toHaveAccessibilityState } from './to-have-accessibility-state';
-import { toHaveAccessibilityValue } from './to-have-accessibility-value';
 
 expect.extend({
   toBeDisabled,
@@ -15,11 +15,11 @@ expect.extend({
   toBeEmptyElement,
   toBeEmpty, // Deprecated
   toBeOnTheScreen,
+  toBeVisible,
   toContainElement,
+  toHaveAccessibilityState,
+  toHaveAccessibilityValue,
   toHaveProp,
   toHaveStyle,
   toHaveTextContent,
-  toBeVisible,
-  toHaveAccessibilityState,
-  toHaveAccessibilityValue,
 });
